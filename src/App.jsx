bedcf7378abc669baaca2c36896a334f807622e1
Wichakorn <s6752410002@sau.ac.th>
2025-11-11 17:07:52 +0700

    V.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ShowAllKinKun from './pages/ShowAllKinkun';
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ShowAllKinKun from "./pages/ShowAllKinkun";
+import AddKinkun from "./pages/AddKinkun";
+import EditKinkun from "./pages/EditKinkun";
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/showallkinkun" element={<ShowAllKinKun />} />
+        <Route path="/addkinkun" element={<AddKinkun />} />
+        <Route path="/editkinkun/:id" element={<EditKinkun />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/ShowAllKinkun.jsx
+++ src/pages/ShowAllKinkun.jsx
@@ -1,100 +1,183 @@
-import react, { use } from 'react'
-import food from '../images/food.png'
-import { Link } from 'react-router-dom';
-import Footer from '../components/Footer'
-import { useEffect, useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import react, { use } from "react";
+import food from "../images/food.png";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import Swal from "sweetalert2";
 
-export default function ShowAllKinKun(){
+export default function ShowAllKinKun() {
+  const [kinkuns, setkinkuns] = useState([]);
 
-    const [kinkuns, setkinkuns] = useState([]);
-
-    useEffect(() => {
-        try{
-        //โค้ดที่จะให้ทํางานเมื่อมี Effect เกิดขึ้นกับ Component
-        //ดึงข้อมูลการกินทั้งหมดจาก Supabase  
-        const fetchKinkuns = async () => {
-            //ดึงจาก Supabase
-            const {data, error} = await supabase
-                                .from('kinkun_tb')
-                                .select('*')
-                                .order('created_at', { ascending: false });
+  useEffect(() => {
+    try {
+      //โค้ดที่จะให้ทํางานเมื่อมี Effect เกิดขึ้นกับ Component
+      //ดึงข้อมูลการกินทั้งหมดจาก Supabase
+      const fetchKinkuns = async () => {
+        //ดึงจาก Supabase
+        const { data, error } = await supabase
+          .from("kinkun_tb")
+          .select("*")
+          .order("created_at", { ascending: false });
         // ดึงมาเเล้ว ให้ตรวจสอบ error
-        if(error){
-            alert('เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง');
-            console.log('Fetch error:', error);
-        }else{
-            //เอาค่าที่ดึงมาไปเก็บไว้ใน state
-            setkinkuns(data);
+        if (error) {
+          //alert("เกิดข้อผิดพลาด กรุณาลองใหม่อีกครั้ง");
+          Swal.fire({
+            icon: "warning",
+            iconColor: "orange",
+            title: "เกิดข้อผิดพลาดในการดึงข้อมูล กรุณาลองใหม่อีกครั้ง",
+            showConfirmButton: true,
+            confirmButtonText: "ตกลง",
+            confirmButtonColor: "orange",
+          });
+          console.log("Fetch error:", error);
+        } else {
+          //เอาค่าที่ดึงมาไปเก็บไว้ใน state
+          setkinkuns(data);
         }
+      };
+      //เรียกใช้ฟังก์ชันดึงข้อมูล
+      fetchKinkuns();
+    } catch (error) {
+      alert("เกิดช้อผิดพลาด กรุณาลองใหม่อีกครั้ง");
+      console.log("Fetch error:", error);
     }
-        //เรียกใช้ฟังก์ชันดึงข้อมูล
-        fetchKinkuns();
-        }catch(error){
-            alert("เกิดช้อผิดพลาด กรุณาลองใหม่อีกครั้ง");
-            console.log('Fetch error:', error);
+  }, []);
+
+  //สร้างฟัง์ชันลบข้อมูลออกจาก table และ storage บน Supabase
+  const handleDeleteClick = async (id, food_image_url) => {
+    // แสดงข้อความยืนยันการลบข้อมูล
+    const result = await Swal.fire({
+      icon: "question",
+      iconColor: "#E81A07",
+      title: "คุณแน่ใจหรือไม่ว่าต้องการลบข้อมูลการกินนี้ ?",
+      showConfirmButton: true,
+      confirmButtonText: "ตกลง",
+      confirmButtonColor: "#E81A07",
+      showCancelButton: true,
+      cancelButtonText: "ยกเลิก",
+      cancelButtonColor: "#3085d6",
+    });
+
+    //ตรวจสอบ result ว่าผู้ใช้เลือกตกลง หรือยกเลิก
+    if (result) {
+      // ลบรูปออกจาก storage บน supabase ถ้ามี
+      if (food_image_url != "") {
+        //ตัดเอาแค่ชื่อรูป
+        const image_name = food_image_url.split("/").pop();
+
+        const { error } = await supabase.storage
+          .from("kinkun_bk")
+          .remove([image_name]);
+
+        if (error) {
+          alert("เกิดข้อผิดพลาดในการลบรูปภาพ กรุณาลองใหม่อีกครั้ง");
+          return;
         }
-    }, []);
+      }
 
-    return(
-        <div>
-            <div className='w-10/12 mx-auto border-gray-300 p-6 shadow-md my-10 rounded-1g'>
-                        <h1 className='text-2x1 font-bold text-center text-blue-700'>
-                            Kinkun APP (Supabase)
-                        </h1>
-            
-                        <h1 className='text-2x1 font-bold text-center text-blue-700'>
-                            บันทึกกการกิน
-                        </h1>
-            
-                        <img src={food} alt="กินกัน" className='block mx-auto w-30 mt-5'></img>
+      // ลบข้อมูลออกจาก table บน supabase
+      const { error } = await supabase.from("kinkun_tb").delete().eq("id", id);
 
-                        {/* สวนเเสดงปุ่มเพิ่ม เพื่อเปิดหน้าจอ /addkinkun */}
-                        <div className='my-8 flex justify-end'>
-                            <Link to="/addkinkun" className='bg-blue-700 p-3 rounded hover:bg-blue-800 text-white'>
-                             เพิ่มการกิน
-                            </Link>
-                        </div>
+      if (error) {
+        alert("เกิดข้อผิดพลาดในการลบข้อมูล กรุณาลองใหม่อีกครั้ง");
+        return;
+      }
 
-                        {/* สวนเเสดงข้อมูลการกินทั้งหมดที่ดึงมาจาก Supabase โดยเเสดงเป็นตาราง*/}
-                        <table className='w-full border border-gray-700 text-sm'>
-                            <thead>
-                                <tr className='bg-gray-300'>
-                                    <th className='border border-gray-700 p-2'>รูป</th>
-                                    <th className='border border-gray-700 p-2'>กินอะไร</th>
-                                    <th className='border border-gray-700 p-2'>กินที่ไหน</th>
-                                    <th className='border border-gray-700 p-2'>กินไปเท่าไร</th>
-                                    <th className='border border-gray-700 p-2'>วันไหน</th>
-                                    <th className='border border-gray-700 p-2'>ACTION</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {kinkuns.map((kinkun)=>(
-                                    <tr key={kinkun.id} >
-                                        <td className='border border-gray-700 p-2'>
-                                            {
-                                                kinkun.food_image_url == '' || kinkun.food_image_url == null
-                                                ? '-'
-                                                : <img src={kinkun.food_image_url} alt="รูปอาหาร" className='w-20 mx-auto' />
-                                            }
-                                        </td>
-                                        <td className='border border-gray-700 p-2'>{kinkun.food_name}</td>
-                                        <td className='border border-gray-700 p-2'>{kinkun.food_where}</td>
-                                        <td className='border border-gray-700 p-2'>{kinkun.food_pay}</td>
-                                        <td className='border border-gray-700 p-2'>
-                                            {new Date(kinkun.created_at).toLocaleDateString('th-TH')}
-                                        </td>
-                                        <td className='border border-gray-700 p-2'>
-                                            เเก้ไข | ลบ
-                                        </td>
-                                    </tr>
-                                    ))}
-                            </tbody>
-                        </table>
+      alert("ลบข้อมูลการกินเรียบร้อยแล้ว");
 
-            </div>
+      //ลบข้อมูลออกจาก UI
+      setKinkuns(kinkuns.filter((kinkun) => kinkun.id !== id));
+      //หรือ window.location.reload()
+    }
+  };
 
-            <Footer/>
+  return (
+    <div>
+      <div className="w-10/12 mx-auto border-gray-300 p-6 shadow-md my-10 rounded-1g">
+        <h1 className="text-2x1 font-bold text-center text-blue-700">
+          Kinkun APP (Supabase)
+        </h1>
+
+        <h1 className="text-2x1 font-bold text-center text-blue-700">
+          ข้อมูลกการกิน
+        </h1>
+
+        <img src={food} alt="กินกัน" className="block mx-auto w-30 mt-5"></img>
+
+        {/* สวนเเสดงปุ่มเพิ่ม เพื่อเปิดหน้าจอ /addkinkun */}
+        <div className="my-8 flex justify-end">
+          <Link
+            to="/addkinkun"
+            className="bg-blue-700 p-3 rounded hover:bg-blue-800 text-white"
+          >
+            เพิ่มการกิน
+          </Link>
         </div>
-    );
+
+        {/* สวนเเสดงข้อมูลการกินทั้งหมดที่ดึงมาจาก Supabase โดยเเสดงเป็นตาราง*/}
+        <table className="w-full border border-gray-700 text-sm">
+          <thead>
+            <tr className="bg-gray-300">
+              <th className="border border-gray-700 p-2">รูป</th>
+              <th className="border border-gray-700 p-2">กินอะไร</th>
+              <th className="border border-gray-700 p-2">กินที่ไหน</th>
+              <th className="border border-gray-700 p-2">กินไปเท่าไร</th>
+              <th className="border border-gray-700 p-2">วันไหน</th>
+              <th className="border border-gray-700 p-2">ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {kinkuns.map((kinkun) => (
+              <tr key={kinkun.id}>
+                <td className="border border-gray-700 p-2">
+                  {kinkun.food_image_url == "" ||
+                  kinkun.food_image_url == null ? (
+                    "-"
+                  ) : (
+                    <img
+                      src={kinkun.food_image_url}
+                      alt="รูปอาหาร"
+                      className="w-20 mx-auto"
+                    />
+                  )}
+                </td>
+                <td className="border border-gray-700 p-2">
+                  {kinkun.food_name}
+                </td>
+                <td className="border border-gray-700 p-2">
+                  {kinkun.food_where}
+                </td>
+                <td className="border border-gray-700 p-2">
+                  {kinkun.food_pay}
+                </td>
+                <td className="border border-gray-700 p-2">
+                  {new Date(kinkun.created_at).toLocaleDateString("th-TH")}
+                </td>
+                <td className="border border-gray-700 p-2">
+                  <Link
+                    className="text-green-500 underline mx-2 cursor-pointer"
+                    to={"/editkinkun/" + kinkun.id}
+                  >
+                    เเก้ไข
+                  </Link>
+                  |
+                  <button
+                    className="text-red-500 underline mx-2 cursor-pointer"
+                    onClick={() =>
+                      handleDeleteClick(kinkun.id, kinkun.food_image_url)
+                    }
+                  >
+                    ลบ
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
